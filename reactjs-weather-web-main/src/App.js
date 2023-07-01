a487b1f7b5c6c742c914dc798aeb71b43a6f460f
eboshnjaku <eltonboshnjaku@hotmail.com>
2023-07-01 21:37:42 +0200

    app

--- reactjs-weather-web-main/src/App.js
+++ reactjs-weather-web-main/src/App.js
@@ -5,8 +5,8 @@ import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import DashboardComments from "./components/comments/DashboardComments";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Dashboardlogin from "./components/dashboard/Dashboardlogin";
 
 const App = () => {
@@ -15,9 +15,8 @@ const App = () => {
   const [location, setLocation] = useState("");
   const [countries, setCountries] = useState([""]);
   const navigate = useNavigate();
-/*
+
   useEffect(() => {
-    
     const success = (position) => {
       setLat(position.coords.latitude);
       setLong(position.coords.longitude);
@@ -32,24 +31,31 @@ const App = () => {
           setLocation(data.city);
           navigate(`/weather/${data.city}`);
         });
-		 axios.get('http://localhost:3000/airpolution/'+position.coords.latitude+"/"+position.coords.longitude).then(response=>{
-        console.log("polutioni",response.data)
-        if (response.data.list[0].main.aqi) {
-          const aqi = response.data.list[0].main.aqi;
-          if (aqi === 1) {
-            toast.success('Air quality is good')
-          } else if (aqi === 2) {
-            toast.warning('Air quality is moderate')
-          } else {
-            toast.error('Air quality is poor');
+      axios
+        .get(
+          "http://localhost:3000/airpolution/" +
+            position.coords.latitude +
+            "/" +
+            position.coords.longitude
+        )
+        .then((response) => {
+          console.log("polutioni", response.data);
+          if (response.data.list[0].main.aqi) {
+            const aqi = response.data.list[0].main.aqi;
+            if (aqi === 1) {
+              toast.success("Air quality is good");
+            } else if (aqi === 2) {
+              toast.warning("Air quality is moderate");
+            } else {
+              toast.error("Air quality is poor");
+            }
           }
-        }
-        console.log("pi")
-      }).catch(error=>{
-        console.log(error)
-      })
+          console.log("pi");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     };
-    
 
     const error = (error) => {
       console.log(error);
@@ -57,7 +63,7 @@ const App = () => {
 
     navigator.geolocation.getCurrentPosition(success, error);
   }, []);
-*/
+
   useEffect(() => {
     axios
       .get("https://countriesnow.space/api/v0.1/countries/capital")
@@ -67,17 +73,20 @@ const App = () => {
   }, []);
 
   return (
-  <>
-    <Routes>
-      <Route
-        path="/weather/:location"
-        element={<MainScreen countries={countries} />}
-      ></Route>
-      <Route path="/dashboard/comments" element={<DashboardComments />}></Route>
-      <Route path="/dashboard" element={<Dashboardlogin />}></Route>
-    </Routes>
-	    <ToastContainer />
-	</>
+    <>
+      <Routes>
+        <Route
+          path="/weather/:location"
+          element={<MainScreen countries={countries} />}
+        ></Route>
+        <Route
+          path="/dashboard/comments"
+          element={<DashboardComments />}
+        ></Route>
+        <Route path="/dashboard" element={<Dashboardlogin />}></Route>
+      </Routes>
+      <ToastContainer />
+    </>
   );
 };
 
